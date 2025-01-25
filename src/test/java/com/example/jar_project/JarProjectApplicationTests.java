ecaebb4dd06cc8b2877f1858d9e1e17aa4e1ea06
yoko-Developer <whitesnow.wave@gmail.com>
2025-01-25 12:26:30 +0900

    ファイル変更

---
+++ src/test/java/com/example/jar_project/JarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jar_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
